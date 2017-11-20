d82b08f18bed14bf5df6cfba496ed7571ecb232c
Aleks048 <ashum048@uottawa.ca>
2017-11-19 21:29:20 -0500

    InputSeparated

--- CSI2110_Algorithms/assignment4/assignment4/src/main/java/assignment4/inputData.java
+++ CSI2110_Algorithms/assignment4/assignment4/src/main/java/assignment4/inputData.java
@@ -4,24 +4,43 @@ import java.lang.String;
 import java.io.FileReader;
 import java.util.Scanner;
 import java.io.FileNotFoundException;
+import java.util.Vector;
 
 public class inputData{
 
     public static void input() throws FileNotFoundException{
-        System.out.println("HI there");
-     Scanner inputSc = new Scanner (new FileReader("parisSubway.txt")); 
-     String input;
+        
+     Scanner inputSc = new Scanner (new FileReader("C:/Users/Aleks048/Downloads/studies_programming/git/all_programming/CSI2110_Algorithms/assignment4/assignment4/src/main/java/assignment4/parisSubway.txt")); 
+     String numOfVertEdg = inputSc.nextLine();
+     System.out.println(numOfVertEdg);
+
+     Vector<String[]> inputStations = new Vector <String[]>();
+     Vector<String[]> inputDistances = new Vector <String[]>();
+
      while (inputSc.hasNextLine()){
-         input = inputSc.nextLine();
-         System.out.println(input);
+         String temp = inputSc.nextLine();
+         String [] tempArr = temp.split(" ");
+         if (temp.equals("$")){break;}
+         inputStations.add(tempArr);
      }
+    
+
+     while(inputSc.hasNextLine()){
+        String temp = inputSc.nextLine();
+        String [] tempArr = temp.split(" ");
+        inputDistances.add(tempArr);
+     }
+     System.out.println(inputDistances);
     }
+    
 
     public static void main (String args[]){
         try{
         inputData.input();
         }
-        catch (FileNotFoundException e){}
+        catch (FileNotFoundException e){
+            System.out.println("It's not the file you're looking for.");
+        }
         
     } 
 }
