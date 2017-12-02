06a1cf11b8c22ecaca27f1c2a49358764875769a
Aleks048 <ashum048@uottawa.ca>
2017-12-02 16:12:58 -0500

    update

---
+++ CSI2110_Algorithms/lab_8/lab_8/src/main/java/ashum048Lab_8/App.java
@@ -0,0 +1,13 @@
+package ashum048Lab_8;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ csi2120_Paradigmes/labs/lab1.pl
@@ -0,0 +1,15 @@
+color(snow,white).
+size(mountains,tall).
+weight(snow,light).
+color(mountains,grey).
+color(grass,green).
+color(trees,green).
+grow(grass,mountains).
+grow(trees,mountains).
+size(trees,tall).
+size(grass,low).
+color(plains,brown).
+grow(wheat,plains).
+color(wheat,yellow).
+size(wheat,low).
+
